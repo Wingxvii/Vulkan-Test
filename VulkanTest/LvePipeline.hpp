06ba8d6a879707c26b09ef1660574befe5898afa
John <John.wang@ontariotechu.net>
2021-03-07 23:48:56 -0500

    tutorial 4:fixed function pipeline stages

--- VulkanTest/LvePipeline.hpp
+++ VulkanTest/LvePipeline.hpp
@@ -7,18 +7,31 @@
 
 
 namespace lve {
-	struct PipelineConfigInfo {};
+	struct PipelineConfigInfo {
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+	};
 
 	class LvePipeline
 	{
 	public:
 		LvePipeline(LveDevice &device, const std::string& vertPath, const std::string& fragPath, const PipelineConfigInfo& configInfo);
-		~LvePipeline() {}
+		~LvePipeline();
 
 		LvePipeline(const LvePipeline&) = delete;
 		void operator=(const LvePipeline&) = delete;
 
-		static PipelineConfigInfo defaultPipelineConfigInfo(int32_t width, uint32_t height);
+		static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 	private:
 		static std::vector<char> readFile(const std::string& filepath);
 
